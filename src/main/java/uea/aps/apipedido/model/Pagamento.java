c6c4410f3fe66adea9d81030c7131612c783f513
João da Mata <jlfilho@uea.edu.br>
2024-01-22 20:53:37 -0400

    Model: Pagamento

---
+++ src/main/java/uea/aps/apipedido/model/Pagamento.java
@@ -0,0 +1,54 @@
+package uea.aps.apipedido.model;
+
+import java.util.Objects;
+
+import uea.aps.apipedido.model.enums.EstadoPedido;
+
+public class Pagamento {
+	private Integer id;
+	private EstadoPedido estado;
+	
+	public Pagamento() {
+		super();
+	}
+
+	public Pagamento(Integer id, EstadoPedido estado) {
+		super();
+		this.id = id;
+		this.estado = estado;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public EstadoPedido getEstado() {
+		return estado;
+	}
+
+	public void setEstado(EstadoPedido estado) {
+		this.estado = estado;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pagamento other = (Pagamento) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
