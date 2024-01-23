16eb67f9c2c449d2055b937ed5b4aa4999a6fd9c
João da Mata <jlfilho@uea.edu.br>
2024-01-22 20:45:08 -0400

    Model: Ciente

---
+++ src/main/java/uea/aps/apipedido/model/Cliente.java
@@ -0,0 +1,88 @@
+package uea.aps.apipedido.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import uea.aps.apipedido.model.enums.TipoCliente;
+
+public class Cliente {
+	private Integer id;
+	private String nome;
+	private String email;
+	private String cpfOuCnpj;
+	private TipoCliente tipo;
+	
+	private List<String> telefones = new ArrayList<>();
+	
+	public Cliente() {
+		super();
+	}
+
+	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.cpfOuCnpj = cpfOuCnpj;
+		this.tipo = tipo;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCpfOuCnpj() {
+		return cpfOuCnpj;
+	}
+
+	public void setCpfOuCnpj(String cpfOuCnpj) {
+		this.cpfOuCnpj = cpfOuCnpj;
+	}
+
+	public TipoCliente getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(TipoCliente tipo) {
+		this.tipo = tipo;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
